ba97812d2bd93447a40b1a06dbdd1bf072f392dc
chanpan <chanpan.nuttaphon@gmail.com>
2017-09-05 19:23:05 +0700

    master

---
+++ assets/js/knex.js
@@ -0,0 +1,18 @@
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: "assets/db.db"
+  },
+  useNullAsDefault: true
+}); 
+knex.schema.createTableIfNotExists('users', function (table) {
+  table.increments();
+  table.string('firstname');
+  table.string('lastname');
+  table.string('tel');
+  table.timestamps();
+}).then(function(){
+    return knex.insert([{name: 'nuttaphon'},{name:'rattana'}]).into('users');
+})
+ 
+ 
---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require("electron");
+const {app, BrowserWindow,ipcMain} = electron;
+
+let mainWin="";
+let childWin="";
+
+app.on("ready", ()=>{
+	mainWin = new BrowserWindow({ width:1200, height:600});
+	mainWin.loadURL(`file://${__dirname}/index.html`);
+	app.on('closed', function () { mainWin = null; app.exit() });
+});
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
